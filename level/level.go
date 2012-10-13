9a23e71ae56e1ec337448704ba9aef5887f290f1
oneslang <oneslang@gmail.com>
2012-10-13 14:53:31 +0800

    add level.go

---
+++ level/level.go
@@ -0,0 +1,17 @@
+// Copyright 2012 The Oneslang Authors.  All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Levels used for identifying the severity of an event.
+package level
+
+const (
+	Off = iota
+	Fatal
+	Error
+	Warn
+	Info
+	Debug
+	Trace
+	All
+)
