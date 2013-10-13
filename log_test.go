05f71aa89d129f1ac223c137e9bd8470f42ea110
Bill Mill <bill.mill@gmail.com>
2013-10-13 16:36:32 -0400

    update the line numbers in the test script

--- log_test.go
+++ log_test.go
@@ -11,7 +11,7 @@ const (
 	Rdate         = `[0-9][0-9][0-9][0-9]/[0-9][0-9]/[0-9][0-9]`
 	Rtime         = `[0-9][0-9]:[0-9][0-9]:[0-9][0-9]`
 	Rmicroseconds = `\.[0-9][0-9][0-9][0-9][0-9][0-9]`
-	Rline         = `(53|61):` // must update if the calls to l.Printf / l.Print below move
+	Rline         = `(55|63):` // must update if the calls to l.Printf / l.Print below move
 	Rlongfile     = `.*/[A-Za-z0-9_\-]+\.go:` + Rline
 	Rshortfile    = `[A-Za-z0-9_\-]+\.go:` + Rline
 	Rinfo         = `INFO `
